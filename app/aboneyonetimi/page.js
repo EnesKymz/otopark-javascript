c00dc3b2a224c084e08d8b1176599c18629737e9
EnesKymz <okkeseneskaymaz@proton.me>
2025-05-17 14:47:15 +0300

    update

--- app/aboneyonetimi/page.js
+++ app/aboneyonetimi/page.js
@@ -1,18 +1,10 @@
 "use client"
 import { useSession } from "next-auth/react";
 import SubscriptionManage from "../components/subscribtionManage";
-import MaintenancePage from "../components/maintenance";
 
 export default function Dashboard(){
    
     const { data: session } = useSession();
-    if(process.env.NEXT_PUBLIC_VERCEL_ENV==="production") {
-        return (
-            <div>
-            <MaintenancePage/>
-            </div>
-        )
-    }
     return(
     <div>
             {session && (
