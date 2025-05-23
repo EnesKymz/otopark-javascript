60ca3e4d4aac1a106c21b7deb1e4caf6543e3221
EnesKymz <okkeseneskaymaz@proton.me>
2025-05-23 19:54:48 +0300

    update

--- app/utils/deviceDetection.jsx
+++ app/utils/deviceDetection.jsx
@@ -4,13 +4,9 @@ import { headers } from 'next/headers';
 
 const deviceTypeDetector = async () => {
     // Await the headers() function
-    const ua = navigator.userAgent.toLowerCase();
-    let deviceType = "desktop"
-    if (ua.includes("wv") || ua.includes("webview") || ua.includes("expo")) {
-        deviceType = "mobile";
-    } else {
-        deviceType = "desktop";
-    }    
+    const headerValues = await headers();
+    const { device } = userAgent({ headers: headerValues });
+    const deviceType = device
     return deviceType;
 };
 
