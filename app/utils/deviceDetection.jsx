670aa8f532be6b7727bbca5a11ad81d49e38d744
EnesKymz <okkeseneskaymaz@proton.me>
2025-05-23 20:11:14 +0300

    update

--- app/utils/deviceDetection.jsx
+++ app/utils/deviceDetection.jsx
@@ -1,15 +1,25 @@
+// app/utils/deviceDetector.ts
 'use server';
-import { userAgent } from 'next/server';
+
 import { headers } from 'next/headers';
 
-const deviceTypeDetector = async () => {
-   const userAgent = req.headers['user-agent'] || '';
-    const isWebView = /(WebView|Facebook|Instagram|Line|Twitter|WhatsApp)/i.test(userAgent);
-    let deviceType = ""
-    if(isWebView){
-        deviceType = "mobile"
-    }
-    return deviceType;
+const deviceTypeDetector = async ()=> {
+  const userAgent = headers().get('user-agent') || '';
+  
+  // WebView kontrolü
+  const isWebView = /(WebView|FBAN|Instagram|Line|Twitter|WhatsApp)/i.test(userAgent);
+  
+  // Mobil cihaz kontrolü
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+  
+  let deviceType = ""
+  
+  if (isWebView || isMobile) {
+    deviceType = 'mobile';
+  } else {
+    deviceType = 'desktop';
+  }
+  
+  return deviceType;
 };
-
-export default deviceTypeDetector;
+export default deviceTypeDetector
